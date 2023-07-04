4329863e45161ae1cb5d84a0f829a0dda32207cc
Alexis Bonneau <60471569+alex892-droid@users.noreply.github.com>
2023-07-04 21:26:25 +0200

    Added class singleton

---
+++ TechnicalToolsSharedKernel/TechnicalToolsSharedKernel/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalToolsSharedKernel
+{
+    public static class Singleton<T>
+        where T : new()
+    {
+        private static T value = new();
+
+        public static T Value { get { return Singleton<T>.value; } }
+    }
+}
